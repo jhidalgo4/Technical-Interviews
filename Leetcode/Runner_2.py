c2d2f2fe2107d647d1383545d9d1908d14b3493a
jhidalgo4 <joaquin.norte37@gmail.com>
2022-06-25 19:56:41 -0600

    init leetcode folders

---
+++ Leetcode/Runner_2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 25 19:55:57 2022
+
+@author: joaqu
+"""
+
---
+++ Leetcode/Runner_3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 25 19:56:06 2022
+
+@author: joaqu
+"""
+
